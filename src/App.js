f87b6476d7928d30a9e50d2c3f6b3184a2868269
HariGadi143 <haridhonigadi0707@gmail.com>
2024-02-29 08:08:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mainContainer">
+        <h1>SuperOverLeague</h1>
+        <div className="teamsContainer">
+          <div className="teamImgCon">
+            <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" alt="rcbImg"/>
+          </div>
+
+          <div className="teamImgCon">
+            <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" alt="cskImg" />
+          </div>
+
+        </div>
+
+      </div>
+
+
     </div>
   );
 }
