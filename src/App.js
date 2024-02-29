05ef8be743cda366829ea5ee6d665a24865d695d
HariGadi143 <haridhonigadi0707@gmail.com>
2024-02-29 08:21:06 +0530

    SuperOverLeague page implemented

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ function App() {
   return (
     <div className="App">
       <div className="mainContainer">
-        <h1>SuperOverLeague</h1>
+        <h1 className="mainHeading">SuperOverLeague</h1>
         <div className="teamsContainer">
           <div className="teamImgCon">
-            <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" alt="rcbImg"/>
+            <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" alt="rcbImg" style={{height : "100%", width: "100%"}}/>
           </div>
 
           <div className="teamImgCon">
-            <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" alt="cskImg" />
+            <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" alt="cskImg" style={{height : "100%", width: "100%"}} />
           </div>
 
         </div>
